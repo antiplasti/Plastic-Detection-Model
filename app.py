15b4e780f55686a6e3f4006ccd59595f0140980b
irontarkus95 <michael.ali1995@gmail.com>
2019-02-10 03:07:04 -0400

    flask server for digital ocean droplet

--- app.py
+++ app.py
@@ -16,24 +16,18 @@ def health_check():
 # Performing image Recognition on Image, sent as bytes via POST payload
 @app.route('/detect', methods=["POST"])
 def detect():
-    try:
-        # Converts bytes, received via request, to Pillow Image Object
-        pil_img = Image.open(BytesIO(request.data))
 
-        # Pass image bytes to classifier
-        result = classify.analyse(pil_img)
+    # Converts bytes, received via request, to Pillow Image Object
+    pil_img = Image.open(request.files['file'])
+    print("successfully receieved image")
     
-        # Return results as neat JSON object, using 
-        result = jsonify(result)
-        print(result)
-        return result
-    
-    except:
-        return jsonify(
-            {
-                "Error": "Image could not be read and/or parsed."
-            }
-        )
+    # Pass image bytes to classifier
+    result = classify.analyse(pil_img)
+
+    # Return results as neat JSON object, using 
+    result = jsonify(result)
+    print(result)
+    return result
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
--- service_localtest.py
+++ service_localtest.py
@@ -1,24 +1,12 @@
 import requests
 import io
-from PIL import Image
 
 def postImg():
 
-    imgPath = "testimg.png"
-        
-    imgBytes = io.BytesIO()
-    img = Image.open(imgPath)
-    img.convert('RGB').save(imgBytes, 'png')  
-
-    
-    headers = {
-        'content-type': 'image/jpeg'
-    }
-    
+    imgPath = "@testing.png"    
     response = requests.post(
 		"http://174.138.58.241/detect",
-		data=imgBytes,
-		headers=headers
+		files=imgPath
 	)
 	
     utilData = response.json()
