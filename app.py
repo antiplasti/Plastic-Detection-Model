341a9bc72f80de5ff4c896e9276265b13536dc6c
eugeneverywhere <eugeneverywhere@gmail.com>
2020-03-10 17:22:11 +0300

    env port

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 from flask import Flask, request, jsonify
 import classify
 import base64
+import os
+
 import json
 import firebase
 import env
@@ -37,13 +39,8 @@ def detect():
 
     response_data = result.json
     print(response_data)
-    
-    db = firebase.Firebase()
-    db.authenticate()
-    db.push(response_data)
-    print("Updated Firebase.")
 
     return result
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=os.environ['PORT'], debug=True)
